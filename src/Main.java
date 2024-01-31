35520b5dcd589a19dd3a2c263d34741294477ab8
Abhishek Galfade <abhishekgalfade@vasyerp.com>
2024-01-31 14:57:06 +0530

    Pahela commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String url="jdbc:postgresql://localhost:5432/Demo";
+        String username="postgres";
+        String password="postgres";
+        String sql = "select sname from student where sid= 1";
+        Class.forName("org.postgresql.Driver");
+        Connection con= getConnection(url,username,password);
+        Statement st=con.createStatement();
+        System.out.println("Connection Established");
+        st.executeQuery(sql);
+        ResultSet rs = st.executeQuery(sql);
+        rs.next();
+        String name = rs.getString("sname");
+        System.out.println("Name is " + name);
+       // System.out.println(rs.next());
+        con.close();
+        System.out.println("Connection closed");
+    }
+}
